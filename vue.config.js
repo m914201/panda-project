5468f4bd3b481984509a6970972ccb1da8b857a6
Lim JinHyeok <m914201@naver.com>
2023-03-21 14:53:48 +0900

    eslint 기본 셋팅 scss 기본 셋팅

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path')
+
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src/'),
+        Views: path.join(__dirname, 'src/views'),
+        Components: path.join(__dirname, 'src/components'),
+        assets: path.join(__dirname, 'src/assets'),
+        Mixins: path.join(__dirname, 'src/mixins'),
+        Api: path.join(__dirname, 'src/api'),
+        Configs: path.join(__dirname, 'src/configs'),
+        Common: path.join(__dirname, 'src/components/common'),
+      },
+    },
+  },
+}
