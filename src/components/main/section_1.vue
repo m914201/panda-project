3e0420321085358f0261c6d72db0dff7a8c6648b
Lim Jin Hyeok <m914201@naver.com>
2023-03-29 17:38:04 +0900

    포트폴리오 section_1 작업중

--- src/components/main/section_1.vue
+++ src/components/main/section_1.vue
@@ -1,7 +1,26 @@
 <template>
   <div class="main-section section_1">
     <div class="item-warp">
-      <div id="glassmorphism"></div>
+      <div id="glassmorphism">
+        <vue-typer
+          class="typing-display"
+          :text="[
+            '안되면 될 때까지,',
+            '누구보다 많이 뛰어서,',
+            '한번 시작하면 끝을 보자',
+          ]"
+          :repeat="0"
+          :shuffle="false"
+          initial-action="typing"
+          :pre-type-delay="500"
+          :type-delay="70"
+          :pre-erase-delay="800"
+          :erase-delay="100"
+          erase-style="select-back"
+          :erase-on-complete="false"
+          caret-animation="blink"
+        ></vue-typer>
+      </div>
       <div class="stars">
         <div class="star" v-for="(star, idx) in 50" :key="`star_${idx}`"></div>
       </div>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueTyperPlugin from 'vue-typer'
 import 'Api/firebase'
 
+Vue.use(VueTyperPlugin)
 Vue.config.productionTip = false
 
 new Vue({
