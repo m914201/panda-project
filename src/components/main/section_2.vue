7b2e8b188ff53546e0779c3f703b1ca45dc48faa
Lim Jin Hyeok <m914201@naver.com>
2023-03-31 17:03:59 +0900

    section2번 추가

---
+++ src/components/main/section_2.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main-section section_2">
+    <div class="item-warp">
+      <font-awesome-icon :icon="['fas', 'user']" />
+      <font-awesome-icon icon="fa-solid fa-user" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    // popup,
+  },
+  computed: {},
+  methods: {},
+  async created() {},
+  updated() {},
+}
+</script>
+
+<style></style>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,14 +1,17 @@
 <template>
   <div class="page-item-wrap" ref="page-item-wrap">
     <section_1 class="section fullpage" ref="section1"></section_1>
+    <section_2 class="section fullpage" ref="section2"></section_2>
   </div>
 </template>
 
 <script>
 import section_1 from 'Components/main/section_1'
+import section_2 from 'Components/main/section_2'
 export default {
   components: {
     section_1,
+    section_2,
   },
   mixins: [],
   data() {
