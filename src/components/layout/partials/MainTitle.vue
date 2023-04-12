808376f43564d56b2471c02a351bafcbf36e9d89
Lim Jin Hyeok <m914201@naver.com>
2023-04-12 14:54:50 +0900

    content title관련 component업로드

---
+++ src/components/layout/partials/MainTitle.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="tit" :class="title">
+    <h1>{{ titleTxt }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    titleTxt: {
+      type: String,
+      default: '',
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
+
+<style></style>
