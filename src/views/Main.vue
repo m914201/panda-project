1a481145525979b46abdb9c21c6a762417f379cc
Lim Jin Hyeok <m914201@naver.com>
2023-04-12 14:52:32 +0900

    skill관련 아이콘 이미지 업로드

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,26 +1,37 @@
 <template>
   <div class="page-item-wrap" ref="page-item-wrap" @mouseWheelHandle="onScroll">
     <main-header @scrollInto="scrollInto" :navState="navState"></main-header>
-    <section_1 class="section section_1 fullpage" ref="Skill"></section_1>
-    <!--<section_2 class="section section_2 fullpage" ref="TimeLine"></section_2>
-    <section_4 class="section section_4 fullpage" ref="TimeLine"></section_4> -->
+    <section_1 class="section section_1 fullpage" ref="Home"></section_1>
+    <section_2
+      class="section section_2 fullpage"
+      ref="Skill"
+      :invasion="invasion"
+    ></section_2>
+    <section_3
+      class="section section_2 fullpage"
+      ref="TimeLine"
+      :invasion="invasion"
+    ></section_3>
   </div>
 </template>
 
 <script>
 import MainHeader from 'Components/layout/MainHeader'
 import section_1 from 'Components/main/section_1'
+import section_2 from 'Components/main/section_2'
+import section_3 from 'Components/main/section_3'
 export default {
   components: {
     MainHeader,
     section_1,
-    // section_2,
-    // section_4,
+    section_2,
+    section_3,
   },
   mixins: [],
   data() {
     return {
       navState: 'Home',
+      invasion: 'Home',
     }
   },
   methods: {
@@ -32,6 +43,7 @@ export default {
     this.ScrollY = window.pageYOffset
     window.addEventListener('scroll', this.onScroll)
     this.calculateSectionOffsets()
+    this.TitleOffsets()
   },
   beforeDestroy() {
     window.removeEventListener('scroll', this.onScroll)
