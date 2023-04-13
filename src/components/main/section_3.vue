8c254f6fa6ab27feabca398e4a3b15f9ef2ffb05
Lim Jin Hyeok <m914201@naver.com>
2023-04-13 16:15:39 +0900

    section3번 추가

--- src/components/main/section_3.vue
+++ src/components/main/section_3.vue
@@ -1,13 +1,27 @@
 <template>
-  <div class="main-section section_2">
+  <div class="main-section section_3">
     <div class="item-warp">
       <article class="inner">
         <main-title
           :titleTxt="targetStep.titleTxt"
           :title="targetStep.title"
         ></main-title>
-        <div class="content skill-wrap">
-          <div class="time-gubun"></div>
+        <div class="content time-wrap">
+          <div class="time-gubun">
+            <div class="time-wrap">
+              <div
+                class="cont-wrap"
+                v-for="(list, ix) in timeline"
+                :key="`test_${ix}`"
+              >
+                <div class="date">{{ list.date }}</div>
+                <div class="Neumorphism">
+                  <div class="company">{{ list.cont }}</div>
+                  <div class="Language">{{ list.lang }}</div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </article>
     </div>
@@ -16,13 +30,15 @@
 
 <script>
 import MainTitle from 'Components/layout/partials/MainTitle'
-import { MAIN_TITLE } from 'Configs/layout.js'
+import { MAIN_TITLE, TIMELINE } from 'Configs/layout.js'
 export default {
   components: {
     MainTitle,
   },
   data() {
-    return {}
+    return {
+      timeline: Object.freeze(TIMELINE),
+    }
   },
   props: {},
   computed: {
