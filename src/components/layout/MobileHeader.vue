fe94dea788233f204aa0d08628966f24a8b27f6a
Lim Jin Hyeok <m914201@naver.com>
2023-04-27 16:32:59 +0900

    모바일 헤더 추가

---
+++ src/components/layout/MobileHeader.vue
@@ -0,0 +1,69 @@
+<template>
+  <header>
+    <div class="rela">
+      <div id="header-top" :class="{ active: trigger }">
+        <div class="l-wrap">
+          <a href="javascript:void(0)" class="btn-home">
+            <svg-logo
+              :class="[{ on: navState !== 'Home' }, { active: trigger }]"
+            ></svg-logo>
+            <p :class="[{ on: navState !== 'Home' }, { active: trigger }]">
+              LJH Portfolio
+            </p>
+          </a>
+        </div>
+        <div
+          class="ham-wrapp"
+          @click="menuTrigger"
+          :class="[{ active: trigger }]"
+        >
+          <svg-ham
+            class="ham"
+            :class="[{ on: navState !== 'Home' }, { active: trigger }]"
+          ></svg-ham>
+        </div>
+      </div>
+      <div
+        class="gnb-item-wrap"
+        @click="menuTrigger"
+        :style="headerStyle"
+        :class="[{ trigger: trigger }]"
+      >
+        <m-gnb-list @scrollInto="scrollInto" :navState="navState"></m-gnb-list>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+import MGnbList from 'Components/layout/partials/MGnbList'
+import SvgLogo from 'Components/svg/SvgLogo'
+import SvgHam from 'Components/svg/SvgHam'
+export default {
+  data() {
+    return {
+      trigger: false,
+    }
+  },
+  props: {
+    navState: {
+      type: String,
+    },
+  },
+  components: {
+    MGnbList,
+    SvgLogo,
+    SvgHam,
+  },
+  methods: {
+    scrollInto(title) {
+      this.$emit('scrollInto', title)
+    },
+    menuTrigger() {
+      this.trigger = !this.trigger
+    },
+  },
+}
+</script>
+
+<style></style>
---
+++ src/components/layout/partials/MGnbList.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul>
+    <li
+      v-for="(list, idx) in gnbList"
+      :key="`gnb_${idx}`"
+      :ref="list.title"
+      @click="scrollInto(list.title)"
+      :class="[{ on: list.on || list.title == navState }]"
+    >
+      <a href="javascript:void(0);" :style="headerColor">{{ list.title }}</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+import { GNB_LIST } from 'Configs/layout.js'
+export default {
+  data() {
+    return {
+      gnbList: JSON.parse(JSON.stringify(GNB_LIST)),
+    }
+  },
+  props: {
+    navState: {
+      type: String,
+    },
+  },
+  watch: {
+    navState() {
+      if (this.navState == 'Home') this.isActive = false
+      else this.isActive = true
+    },
+  },
+  methods: {
+    scrollInto(title) {
+      this.$emit('scrollInto', title)
+      console.log(title)
+    },
+  },
+}
+</script>
+
+<style></style>
