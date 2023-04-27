2a190b32803178b57f44747e04c04683b01259ef
Lim Jin Hyeok <m914201@naver.com>
2023-04-27 16:36:04 +0900

    1. section5,6번 추가 2.반응형 header 추가 3.높이 값 viewport에 따라 vh로 변수 값 지정

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,38 +1,63 @@
 <template>
   <div class="page-item-wrap" ref="page-item-wrap" @mouseWheelHandle="onScroll">
-    <main-header @scrollInto="scrollInto" :navState="navState"></main-header>
-    <section_1 class="section section_1 fullpage" ref="Home"></section_1>
+    <main-header
+      v-if="windowWid"
+      @scrollInto="scrollInto"
+      :navState="navState"
+    ></main-header>
+    <mobile-header
+      v-else
+      @scrollInto="scrollInto"
+      :navState="navState"
+    ></mobile-header>
+    <section_1 class="section fullpage" ref="Home"></section_1>
     <section_2
-      class="section section_2 fullpage"
+      class="section fullpage"
       ref="Skill"
       :invasion="invasion"
     ></section_2>
     <section_3
-      class="section section_3 fullpage"
+      class="section fullpage"
       ref="TimeLine"
       :invasion="invasion"
     ></section_3>
     <section_4
-      class="section section_4 fullpage"
+      class="section fullpage"
       ref="Project"
       :invasion="invasion"
     ></section_4>
+    <section_5
+      class="section fullpage"
+      ref="Additional"
+      :invasion="invasion"
+    ></section_5>
+    <section_6
+      class="section fullpage"
+      ref="Footer"
+      :invasion="invasion"
+    ></section_6>
   </div>
 </template>
 
 <script>
 import MainHeader from 'Components/layout/MainHeader'
+import MobileHeader from 'Components/layout/MobileHeader'
 import section_1 from 'Components/main/section_1'
 import section_2 from 'Components/main/section_2'
 import section_3 from 'Components/main/section_3'
 import section_4 from 'Components/main/section_4'
+import section_5 from 'Components/main/section_5'
+import section_6 from 'Components/main/section_6'
 export default {
   components: {
     MainHeader,
+    MobileHeader,
     section_1,
     section_2,
     section_3,
     section_4,
+    section_5,
+    section_6,
   },
   mixins: [],
   data() {
@@ -51,9 +76,16 @@ export default {
     window.addEventListener('scroll', this.onScroll)
     this.calculateSectionOffsets()
     this.TitleOffsets()
+    let vh = window.innerHeight * 0.01
+
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
   },
   beforeDestroy() {
     window.removeEventListener('scroll', this.onScroll)
+  },
+  created() {
+    window.addEventListener('resize', this.handleResize)
+    this.handleResize()
   },
 }
 </script>
